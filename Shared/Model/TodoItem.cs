9dae24a582ec3b926166c11523b80ab3545f16ae
iqrafatimame <iqrafatimame@gmail.com>
2021-12-15 20:11:21 +0500

    ✨ Added EDIT TODO Functionality

---
+++ Shared/Model/TodoItem.cs
@@ -0,0 +1,10 @@
+
+public class TodoItem {
+    public int id { get; set; }
+    public DateTime deadline { get; set; }
+    public bool isEdited { get; set; } = false;
+    public string Title { get; set; } = "/";
+    public bool isCompleted { get; set; }
+    
+}
+
